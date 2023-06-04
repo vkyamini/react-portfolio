d3846ad8827f81204032815962dbbe4b7ee7707c
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-04 12:29:25 -0700

    added the contents

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from "../../assets/myimage.jpg"
 
 export default function Home(){
     const styles={
@@ -15,10 +16,25 @@ export default function Home(){
            }
 }
     return(
-        <div styles={styles.img}> <h1>This is home page</h1>
-        <p>This is what my home page looks like</p>
-        <br/>
+     <div>
+       <h1>Welcome , i'm V.K YAMINI</h1>
+        <div>
+        <div>
+        <div>
+      <img src={image} alt="mypic"/>
+    </div>
         </div>
+        <p>I'm currently happy student of the Coding Bootcamp at University of Washington.</p>
+        <p>Other than coding, I also enjoy meditating and volunteering as a <a href="https://heartfulness.org/us/" target="_blank">Heartfulness</a> meditation trainer.</p>
+        <p>I always look forward to learning new things.</p>
+      </div>
+     </div>
+            
+
+
+       
+      
+       
        
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import guruu from "../../assets/guruu.png"
+import cal from "../../assets/cal.png"
+import drty from "../../assets/drty.png"
+import notes from "../../assets/notes.png"
+import portfolio from "../../assets/portfolio.png"
+
+
 
 export default function Projects(){
+
+    const styles={
+        img:{
+            width :"200px",
+            height :"200px"
+        },
+        box:{
+            display:"flex",
+            justifyContent:"space-between"
+         }
+    }
     return(
-        <h1>This is Projects page</h1>
+        
+        <div >
+        <div style={styles.box} >
+        <a href="https://647c12620e4a0e000705f12f--guru-ui.netlify.app/" target="_blank">
+            <div id="work-div">
+            <img style={styles.img} src={guruu} alt="mypic"/>
+              <p class="corner text-left">The-Guruu </p>
+            </div>
+          </a>
+          <a href=" https://drty-nomad-2023.herokuapp.com/." target="_blank">
+            <div class="box-1">
+            <img style={styles.img} src={drty} alt="mypic"/>
+              <p class="corner text-left">Drty Nomads</p>
+            </div>
+          </a>
+          <a href="https://vkyamini.github.io/portfolio/" target="_blank">
+            <div class="box-1">
+            <img style={styles.img} src={portfolio} alt="mypic"/>
+              <p class="corner text-left">Portfolio</p>
+            </div>
+          </a>
+          <a href=" https://addnotes.herokuapp.com/" target="_blank">
+            <div class="box-1">
+            <img style={styles.img} src={notes} alt="mypic"/>
+              <p class="corner text-left">Notes-taker</p>
+            </div>
+          </a>
+          <a href=" https://vkyamini.github.io/daily-Calendar/" target="_blank">
+            <div class="box-1">
+            <img style={styles.img} src={cal} alt="mypic"/>
+              <p class="corner text-left">daily-calendar</p>
+            </div>
+          </a>
+         </div>
+      </div>
+  
     )
 }
